3adba0961d2e38cb7688f88b1e76baa235e1b9cc
uvcanaditya <uvcan.aditya@gmail.com>
2023-09-17 03:19:55 +0530

    Footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import Nav from './Nav';
+import Nav from './components/Nav';
 import { BrowserRouter ,Routes ,Route } from 'react-router-dom';
-
+import Footer from './components/Footer';
 
 
 function App() {
@@ -12,11 +12,12 @@ function App() {
       <Routes>
         <Route path='/' element={<h1>Product list</h1>}></Route>
         <Route path='/add' element={<h1>Add product</h1>}></Route>
-        <Route path='/update' element={<h1>Update Product</h1>}></Route>\
+        <Route path='/update' element={<h1>Update Product</h1>}></Route>
         <Route path='/logout' element={<h1>Log out</h1>}></Route>
         <Route path='/profile' element={<h1>Profile</h1>}></Route>
       </Routes>
       </BrowserRouter>
+      <Footer/>
      </div>
   );
 }
